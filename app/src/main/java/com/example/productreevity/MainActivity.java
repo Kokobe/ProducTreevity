01b3fddaff5021aad383936b4769db5107c4aed2
Varij Jhaveri <varij.jhaveri@gmail.com>
2020-09-18 22:01:28 -0700

    Created new main .java and .xml files

---
+++ app/src/main/java/com/example/productreevity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.productreevity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //hello
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/example/productreevity/TimerMain.java
+++ app/src/main/java/com/example/productreevity/TimerMain.java
@@ -2,11 +2,7 @@ package com.example.productreevity;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
@@ -40,7 +36,7 @@ public class TimerMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         //hello
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.timer_main_activity);
 //        runTimer();
     }
 
